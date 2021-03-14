9dcbb13284869bedd4635188bb2652d8e6f0866e
GitHub Action <action@github.com>
2021-03-14 10:01:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/shykial/intellijplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.shykial.intellijplugintest.services
+
+import com.github.shykial.intellijplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
